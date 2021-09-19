61f795f8c701f48aa3002a7db410022868d2fd1c
whamer100 <whammer100@aol.com>
2021-09-18 21:21:05 -0700

    totally didnt add anything funny

--- src/readma/__init__.py
+++ src/readma/__init__.py
@@ -1,1 +1,6 @@
 from readma.readma import Readma, ReadmaTypes
+
+if __name__ == '__main__':
+    r = Readma(b"Hello there. I think you're running the wrong file!")
+    rm = ReadmaTypes.__members__  # this is to make a certain test not be angry
+    print(r.readall())
